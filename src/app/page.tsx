7f85271bb6f39c3f79f0d859021f5ef9fc294fca
Ewlanna <erika.lundstrom@student.arenakoncernen.se>
2025-03-25 12:56:25 +0100

    small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { Todo } from "@/utils/types";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from "uuid";
 import TodoList from "@/components/TodoList";
 import NewTodoTask from "@/components/NewTodoTask";
-import styled, {createGlobalStyle} from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 //import globalStyle from "@/styles/globalstyle";
 import Header from "@/components/HeaderTodo";
 
 const StyledMain = styled.main`
   align-items: center;
-`
+`;
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #4b5267;
@@ -27,39 +27,40 @@ export default function Home() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodos = (title: any) => {
-      setTodos(currentTodos => {
-        return [
-          ...currentTodos,
-          {id: uuid(), title, completed: false}
-        ]
-      })
-  }
-  
-  const toggleTodos = (id:string, completed: boolean) => {
-    setTodos(currentTodos => {
-      return currentTodos.map(todos => {
-        if(todos.id === id) {
-          return {...todos, completed}
-        }
-        return todos
-      })
-    })
-  }
+    setTodos((currentTodos) => {
+      return [...currentTodos, { id: uuid(), title, done: false }];
+    });
+  };
 
- const deleteTodo = (id: string) => {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todos => todos.id !== id)
-    })
-  }
+  const toggleTodos = (id: string, completed: boolean) => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todos) => {
+        if (todos.id === id) {
+          return { ...todos, done: completed };
+        }
+        return todos;
+      });
+    });
+  };
+
+  const deleteTodo = (id: string) => {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todos) => todos.id !== id);
+    });
+  };
 
   return (
     <>
-    <GlobalStyle/>
-    <StyledMain >
-    <Header/>
-    <NewTodoTask addTodo={addTodos}/>
-    <TodoList todo={todos} toggleTodo={toggleTodos} deleteTodo={deleteTodo}/>
-    </StyledMain>
+      <GlobalStyle />
+      <StyledMain>
+        <Header />
+        <NewTodoTask addTodo={addTodos} />
+        <TodoList
+          todo={todos}
+          toggleTodo={toggleTodos}
+          deleteTodo={deleteTodo}
+        />
+      </StyledMain>
     </>
   );
 }
