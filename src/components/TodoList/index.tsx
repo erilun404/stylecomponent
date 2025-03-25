eb7c54c0aa9cdaa131a76f9dfd563efb195199e1
Ewlanna <erika.lundstrom@student.arenakoncernen.se>
2025-03-25 14:37:58 +0100

    test for versel

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,35 +1,35 @@
 import TodoItem from "../TodoItem";
 import { Todo } from "@/utils/types";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const StyledUl = styled.ul`
-    color: hsl(200, 29%, 90%);
-`
+  color: hsl(200, 29%, 90%);
+`;
 
 type TodoListProps = {
-    todo: Todo[];
-    toggleTodo: (id: string, completed: boolean) => void;
-    deleteTodo: (id:string) => void;
-}
+  todo: Todo[];
+  toggleTodo: (id: string, completed: boolean) => void;
+  deleteTodo: (id: string) => void;
+};
 
-const TodoList = ({todo, toggleTodo, deleteTodo}:TodoListProps) => {
-    return (
-        <StyledUl>
-        <ul>
-            {todo.length === 0 && "No Todos"}
-            {todo.map(todo => {
-                return (
-                    <TodoItem
-                    key={todo.id}
-                    {...todo}
-                    toggleTodo={toggleTodo}
-                    deleteTodo={deleteTodo}
-                    />
-                )
-            })}
-        </ul>
-        </StyledUl>
-    )
-}
+const TodoList = ({ todo, toggleTodo, deleteTodo }: TodoListProps) => {
+  return (
+    <StyledUl>
+      <ul>
+        {todo.length === 0 && "No Todos"}
+        {todo.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+            />
+          );
+        })}
+      </ul>
+    </StyledUl>
+  );
+};
 
-export default TodoList
+export default TodoList;
